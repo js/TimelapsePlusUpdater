f46b9c33fd3ee183eaef18b32338bd87a28c0c36
Johan Sørensen <johan@johansorensen.com>
2012-10-20 23:12:19 +0200

    Initial Commit

---
+++ TimelapsePlusUpdater/JKSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKSAppDelegate.h
+//  TimelapsePlusUpdater
+//
+//  Created by Johan Sørensen on 10/20/12.
+//  Copyright (c) 2012 Johan Sørensen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JKSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TimelapsePlusUpdater/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TimelapsePlusUpdater
+//
+//  Created by Johan Sørensen on 10/20/12.
+//  Copyright (c) 2012 Johan Sørensen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
